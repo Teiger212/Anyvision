b064b7ada7a938e68da1eb5602e6bbc1364f4fbf
Eyal <eyalteiger@gmail.com>
2018-12-02 00:58:29 +0200

    Better import & use on db init

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,9 +1,11 @@
+import mongoose from 'mongoose';
+import bluebird from 'bluebird';
+
 const uri = "mongodb://eteiger:Snowman3!@guessdb-shard-00-00-2u02y.mongodb.net:27017,guessdb-shard-00-01-2u02y.mongodb.net:27017,guessdb-shard-00-02-2u02y.mongodb.net:27017/anyvision?ssl=true&replicaSet=GuessDB-shard-0&authSource=admin&retryWrites=true";
 class mongoDB {
 	
 	constructor() {
-        const mongoose = require('mongoose');
-        mongoose.Promise = require('bluebird');
+        mongoose.Promise = bluebird;
 		this.mongoose = mongoose;
 	}
 
